a9fda366d9ce40da2c65ea59479b4625d6d3aaf0
Username3533 <153109835+Username3533@users.noreply.github.com>
2023-12-22 13:46:39 -0800

    Add files via upload
    More work with If/For relating to lists

--- if_statments.py
+++ if_statments.py
@@ -1,6 +1,6 @@
 #Fun with If
 
-banned_users = ['jack', 'jill', 'mark']
+""" banned_users = ['jack', 'jill', 'mark']
 
 user = "jack"
 if user in banned_users:
@@ -30,4 +30,41 @@ elif age < 18:
 else:
     price = 40
 
-print(f"Because you are {age} years old, your ticket costs ${price}.")
+print(f"Because you are {age} years old, your ticket costs ${price}.") """
+
+""" usernames = ['admin', 'user1', 'maverick', 'bosscat', 'monkeyman'] """
+
+""" for user in usernames:
+    if user == 'admin':
+        print(f'Hello {user}, would you like a status update?')
+    if user != 'admin':
+        print(f'Welcome back {user}, how are you doing today?') """
+
+""" usernames = []
+
+if usernames:
+    for user in usernames:
+        print(f"Hello, {user}!")
+else:
+    print("Admin! We need users!") """
+
+""" new_users = ['User1', 'jack', 'Maverick', 'roger', 'doger']
+
+for user in new_users:
+    if user.lower() in copy_users:
+        print(f"Sorry, {user.title()} is already taken. Please choose another username.")
+    else:
+        print(f"Welcome {user.title()}!") """
+
+""" numbers = [value * 1 for value in range(1, 10)]
+print(numbers)
+
+for value in numbers:
+    if value == 1:
+        print(f"{value}st")
+    elif value == 2:
+        print(f"{value}nd")
+    elif value == 3:
+        print(f"{value}rd")
+    else:
+        print(f"{value}th") """
