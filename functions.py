f89f5e1278e38e7f6168146c0090f7529093275e
Username3533 <153109835+Username3533@users.noreply.github.com>
2024-01-09 10:53:45 -0800

    Add files via upload
    Updates

--- functions.py
+++ functions.py
@@ -9,3 +9,13 @@ def greet_user(username):
   print(f"Hello, {username.title()}!")
 
 greet_user('marshal')
+
+def message():
+  print('I am learning about functions!')
+
+message()
+
+def favorite_book(title):
+  print(f'One of my favorite books is {title.title()}!')
+
+favorite_book('gone with the wind')
