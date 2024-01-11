384d096ae6fcc7dacdfd6260ac0bbb5d5c32b870
Username3533 <153109835+Username3533@users.noreply.github.com>
2024-01-11 12:51:42 -0800

    Add files via upload
    More fun with functions

--- functions.py
+++ functions.py
@@ -28,10 +28,54 @@ make_shirt(size = 'extra medium', text = 'Cool Story Bro')
 make_shirt() """
 
 
-def make_album(artist, album_name, numoftracks = None):
+""" def make_album(artist, album_name, numoftracks = None):
     album = {'artist' : artist, 'album name' : album_name, 'number of tracks' : numoftracks}
     return album
 
-print(make_album('Linkin Park', 'Meteora', 13))
+print(make_album('Linkin Park', 'Meteora', 13)) """
 
 
+
+#uses one list for an action and puts item in a new list
+""" def print_messages(messages, sent_messages):
+    while messages:
+        sent_message = messages.pop()
+        sent_messages.append(sent_message)
+        
+        
+
+messages = ['You are learning Python', 'How long do you think it will be until you are ready for a job?', 'This is a test']
+sent_messages = []
+print_messages(messages[:], sent_messages)
+print(sent_messages)
+print(messages) """
+
+
+#creates a tuple with unlimited args
+""" def create_sandwich(*toppings):
+    print('\nThis sandwich will have:')
+    print(f'- {toppings}')
+
+create_sandwich('turkey', 'lettuice', 'tomatoes', 'mustard')
+create_sandwich('ham', 'cheese')
+create_sandwich('bacon', 'lettuice', 'tomatoes') """
+
+
+#creates dictionary with unlimited args
+""" def build_profile(first, last, **profile):
+    profile['firstname'] = first
+    profile['lastname'] = last
+    return profile
+
+profile = build_profile('scott', 'holder', age=35, sex='male')
+print(profile) """
+
+
+#just ordered a new truck :)
+""" def make_car(model, make, **car):
+    car['model'] = model
+    car['make'] = make
+    return car
+
+new_car = make_car('dodge', 'ram', fully_loaded='Laramie package', color='ruby red')
+print(new_car) """
