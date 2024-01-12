0777b00df7a5e04b61d368bf9dbd23e1e0057e53
Username3533 <153109835+Username3533@users.noreply.github.com>
2024-01-12 12:36:10 -0800

    Add files via upload
    User Class

--- classes.py
+++ classes.py
@@ -1,7 +1,7 @@
 #working with classes
 
 """ class Dog:
-    """ Making a dog """
+    Making a dog
 
     def __init__(self, name, age):
         self.name = name
@@ -22,4 +22,41 @@ gooddog = Dog('Dixie', 12)
 gooddog.sit()
 gooddog.rollover()
 print(f'{gooddog.name}, whos a good puppy?') """
+
+""" class Restaurant:
+    Making a restaurant
+
+    def __init__(self, name, cousine):
+        self.name = name
+        self.cousine = cousine
+
+    def describe_restaurant(self):
+        print(f'{self.name} is a new restaurant that serves {self.cousine}!')
+    
+    def open_restaurant(self):
+        print(f'{self.name} is now open and proudly serving {self.cousine} food!')
+
+Asian_infusion = Restaurant('Asiafu', 'Asian Infusion')
+Asian_infusion.describe_restaurant()
+Asian_infusion.open_restaurant()
+
+continental = Restaurant('Continental', 'Continental')
+continental.describe_restaurant()
+continental.open_restaurant() """
+
+""" class User:
+    
+    def __init__(self, user_name, age, sex, hobby):
+        self.user_name = user_name
+        self.age = age
+        self.sex = sex
+        self.hobby = hobby
+
+    def desc_user(self):
+        print(f'{self.user_name} is a {self.age} year old {self.sex} who enjoys {self.hobby}.')
+
+aeacus = User('Aeacus', 32, 'male', 'gaming')
+korgen = User('Korgen', 33, 'male', 'kickin ass')
+aeacus.desc_user()
+korgen.desc_user() """
 
