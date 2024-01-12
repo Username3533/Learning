5e07b98cc1534d0a7e72787845b83fd76b518f44
Username3533 <153109835+Username3533@users.noreply.github.com>
2024-01-12 11:25:40 -0800

    Add files via upload
    Classy work good sir!

---
+++ classes.py
@@ -0,0 +1,25 @@
+#working with classes
+
+""" class Dog:
+    """ Making a dog """
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def sit(self):
+        print(f'{self.name} is now sitting.')
+
+    def rollover(self):
+        print(f'{self.name} rolls over.')
+
+my_dog = Dog('Sadie', 8)
+my_dog.sit()
+my_dog.rollover()
+
+gooddog = Dog('Dixie', 12)
+
+gooddog.sit()
+gooddog.rollover()
+print(f'{gooddog.name}, whos a good puppy?') """
+
