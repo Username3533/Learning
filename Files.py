6fc125b94d1b27f25a65216c4d3163c29a07f1cd
Username3533 <153109835+Username3533@users.noreply.github.com>
2024-01-14 13:00:54 -0800

    Add files via upload
    Fun with files, writing/reading/appending

---
+++ Files.py
@@ -0,0 +1,66 @@
+#Ch10 - Files and Exceptions
+
+""" with open('pi_million_digits.txt') as file_object:
+    contents = file_object.readlines()
+
+print(contents.strip())
+
+pi_string = ''
+
+for line in contents:
+    pi_string += line.strip()
+
+print(f'{pi_string[:52]}...')
+print(len(pi_string))
+
+birthday =  input('Enter your birthday in the form mmddyy: ')
+if birthday in pi_string:
+    print('Your birthday is included in the first million digits of Pi!')
+else:
+    print('Your birthday is not included in the first millino didgits of Pi. :(') """
+
+""" with open('learning_python.txt') as learningpython:
+    contents = learningpython.readlines()
+
+python = ""
+for line in contents:
+    python += line
+    
+print(python.replace('python', 'Javascript')) """
+
+""" filename = 'programming.txt'
+
+with open(filename, 'r+') as file:
+    file.write('\nI love learning python!')
+    file.write('\nIt has been such a great experience so far!') """
+
+#append a file to collect data from user input
+""" filename = 'guests.txt'
+
+with open(filename, 'a') as file:
+    name = input('What is your name? ')
+    file.write(name)
+    file.write('\n')
+
+print(f'Welcome {name}!') """
+
+#write all the reasons someone finds programming fun, storing the data in a text file on new lines.
+""" filename = 'whyprogramming.txt'
+
+with open(filename, 'a') as file:
+
+
+    active = True
+    while active: 
+        message = input('Why do you like programming? ') 
+        
+        if message == 'quit':
+            print('Thank you for your input!')
+            active = False
+ 
+        else:
+            file.write(message)
+            file.write('\n') """
+
+
+
