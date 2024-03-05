0502087c5a3e0024e0303f32458c61f661085451
Scott <153109835+Username3533@users.noreply.github.com>
2024-03-05 20:59:38 +0100

    Add files via upload

--- lists.py
+++ lists.py
@@ -1,4 +1,4 @@
-#work with lists #review
+#work with lists
 
 """ friends_names = ['richard', 'justin', 'tony']
 print(type(friends_names))
@@ -107,3 +107,34 @@ for number in numbers:
     print(number)
  """
 
+#Compare 2 strings for common substrings algo - Dynamic O(n*m)
+def longest_common_substring(string1, string2):
+    m = len(string1)
+    n = len(string2)
+
+    # Initialize dp table
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+
+    max_length = 0
+    end_index = 0
+
+    # Fill dp table
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if string1[i - 1] == string2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+                if dp[i][j] > max_length:
+                    max_length = dp[i][j]
+                    end_index = i
+            else:
+                dp[i][j] = 0
+
+    start_index = end_index - max_length
+    substring = string1[start_index:end_index]
+
+    return substring
+
+
+string1 = "abcdefg"
+string2 = "xyzabcde"
+print(longest_common_substring(string1, string2))
