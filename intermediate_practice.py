e060ffc47303d5080e7021f0f52903ad91cc3dc8
Scott <153109835+Username3533@users.noreply.github.com>
2024-03-15 09:03:25 +0100

    Add files via upload

---
+++ intermediate_practice.py
@@ -0,0 +1,33 @@
+""" Concepts include:
+Async programming
+Pandas
+Browser automation, DOM manipulation
+Argparse module
+SQLite3
+Convolutional neural networks
+Image recognition
+Coroutines
+Arrays
+table insertion/querying
+Pandas
+NumPy
+Data encryption/manipulation/visualization
+Deep learning
+HTML/HTTP parsing/nav/routing
+Json parsing
+DataFrames
+Log levels
+Logging
+ML/Natural language processing
+Object mapping SQL
+CSV files
+APIs/HTTP requests
+Server-client architecture
+SMTP servers/email auth
+Socket programming
+Asymmetric/symmetric encryption
+POS tagging/Tokenization/Stemming
+Flask framework, dev, integration and APIs
+Webscraping
+ """
+
